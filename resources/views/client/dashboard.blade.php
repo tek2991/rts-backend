02b19f7d9586e8d246aac30cf951a42f45f849db
Tumtin Eliezer Koren <eliezer.kt@gmail.com>
2023-09-11 08:44:08 +0530

    Added vibrate

--- resources/views/client/dashboard.blade.php
+++ resources/views/client/dashboard.blade.php
@@ -248,7 +248,20 @@
                     </p>
                 </a>
             </div>
-
+            
+            {{-- Vibrate --}}
+            <div>
+                <a href="#" class="flex justify-center items-center flex-col" data-modal-target="popup-modal"
+                    data-modal-toggle="popup-modal"z>
+                    <div
+                        class="bg-white shadow-sm rounded-3xl w-16 sm:w-20 aspect-square p-2 hover:p-1 hover:shadow-md transition-all">
+                        <img src="{{ asset('icons/mobile-vibrate-svgrepo-com.svg') }}" alt="Lock Unlock">
+                    </div>
+                    <p class="sm:text-lg text-center leading-none font-semibold text-gray-700 dark:text-gray-200 pt-2">
+                        Vibrate
+                    </p>
+                </a>
+            </div>
 
         </div>
     </div>
