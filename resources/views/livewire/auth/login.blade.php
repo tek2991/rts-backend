02e8430117031e2fc17ce6c50cd80d4667bdda85
Tumtin Eliezer Koren <eliezer.kt@gmail.com>
2023-10-27 21:36:33 +0530

    updated text

--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -176,7 +176,7 @@
             <div class="">
                 <a class="hover:underline transition text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                     href="{{ route('register') }}">
-                    {{ __('Need an account? Register') }}
+                    {{ __('Need an account?') }}
                 </a>
             </div>
 
